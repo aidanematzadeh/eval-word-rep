f74ce490aabf794adae3378ac4c2a6a4fa6161d9
Aida Nematzadeh <aida@cs.toronto.edu>
2016-12-09 18:44:25 -0800

    remove stopwords

--- get_context.py
+++ get_context.py
@@ -5,116 +5,182 @@ Maps words to ids and keeps the dictionary.
 """
 
 import sys
-#import numpy as np
-import collections
+import numpy as np
 from collections import defaultdict
+from stop_words import get_stop_words
 
-def cumulative_dist(vocab, power=0.75, domain=2**31 - 1):
-        """
-        Create a cumulative-distribution table using stored vocabulary word counts for
-        drawing random words in the negative-sampling training routines.
-        Adopted from gensim.
+def get_stopwords(stopwordsfile):
+    stoplist = set(get_stop_words('en'))
+    with open(stopwordsfile, 'r') as f:
+        stopwords = f.read().split(',')
+    for sw in stopwords:
+        stoplist.add(sw)
+    return stoplist
 
-        To draw a word index, choose a random integer up to the maximum value in the
-        table (cum_table[-1]), then finding that integer's sorted insertion point
-        (as if by bisect_left or ndarray.searchsorted()). That insertion point is the
-        drawn index, coming up in proportion equal to the increment at that slot.
-        """
-        vocab_size = len(vocab)
-        cum_dist = zeros(vocab_size, dtype=uint32)
-        # compute sum of all power (Z in paper)
-        normalization = float(sum([self.vocab[word].count**power for word in self.vocab]))
-        cumulative = 0.0
-        for word_index in range(vocab_size):
-            cumulative += self.vocab[self.index2word[word_index]].count**power / train_words_pow
-            self.cum_table[word_index] = round(cumulative * domain)
-        if len(self.cum_table) > 0:
-            assert self.cum_table[-1] == domain
+def cumulative_dist(vocab_counts, power=0.75, domain=2**31 - 1):
+    """
+    Create a cumulative-distribution table using stored vocabulary word counts for
+    drawing random words in the negative-sampling training routines.
+    Adopted from gensim.
+    """
+    vocab_size = len(vocab_counts)
+    vocab_counts = np.array(vocab_counts)
+    cum_dist = np.zeros(vocab_size, dtype=np.uint32)
+    # compute sum of all power (Z in paper)
+    normalization = float(sum(vocab_counts**power))
+    cumulative = 0.0
+    for word_index in range(vocab_size):
+        cumulative += vocab_counts[word_index]**power / normalization
+        cum_dist[word_index] = round(cumulative * domain)
+        #print(word_index, cumulative, round(cumulative * domain))
 
+    if len(cum_dist) > 0:
+        assert cum_dist[-1] == domain
 
-def generate_negative_examples():
+    return cum_dist
 
-def get_positive_context(input_file, wsize):
-    global_counts = defaultdict(lambda: defaultdict(int))
+def get_negative_examples(negative_dist, positive_counts, vocab_size, k=2):
+    """
+    Draw negative examples given the negative distribution.
+    Draw k negative exmples for each postivie one.
+    From gensim: to draw a word index, choose a random integer up to the maximum value in the table (negative_dist[-1]).
+    then finding that integer's sorted insertion point (as if by bisect_left or ndarray.searchsorted()).
+    That insertion point is the drawn index, coming up in proportion equal to the increment at that slot.
+    """
+    negative_counts = defaultdict(lambda: defaultdict(int))
+
+    for w_index in range(vocab_size):
+        # Number of negative examples to generate for w_index.
+        # For each word in the context of w_index, k negative examples is generated.
+        negative_num = (sum(positive_counts[w_index].values()) - positive_counts[w_index][w_index]) * k
+        #print(w_index, "number of negative samples", negative_num)
+
+        positive_examples = set(positive_counts[w_index].keys())
+
+        # Draw negative examples
+        negative_examples = []
+        rand_vec = np.array(np.random.randint(negative_dist[-1], size=negative_num), dtype='uint32')
+        count = 0
+        while len(negative_examples) < negative_num:
+            nw = negative_dist.searchsorted(rand_vec[count])
+            # Consider the word as a negative example if it is not in its context
+            if not nw in positive_examples:
+                negative_examples.append(nw)
+
+            count += 1
+            if count >= negative_num:
+                rand_vec = np.array(np.random.randint(negative_dist[-1], size=negative_num), dtype='uint32')
+                count = 0
+
+        # Add the negative examples to the negative_counts
+        #print(w_index, "negative examples", negative_examples)
+        for neg_index in negative_examples:
+            negative_counts[w_index][neg_index] += 1
+
+        #assert len(set(negative_examples).intersection(positive_examples)) == 0
+        #assert len(negative_examples) == negative_num
+
+    return negative_counts
+
+def get_positive_examples(input_file, wsize, stoplist):
+    bigram_counts = defaultdict(lambda: defaultdict(int))
+    word_counts = defaultdict(int)
+
+    # mapping between a word and its id
     dictionary = dict()
+    reversed_dictionary = dict()
+
     with open(input_file) as f:
         for counter, doc in enumerate(f):
-            if ((counter+1) % 3 == 0):
+            if ((counter+1) % 1000 == 0):
                 print("document processed %d" % counter)
                 print("number of vocabulary %d" % len(dictionary))
-                #print(sorted(dictionary.keys()))
-                break #TODO
 
-            # Getting the words and adding padding
-            words = doc.split()
-            words = ["_PAD_"] * wsize + words + ["_PAD"] * wsize
-            # Collecing the context of each words
-            contexts = defaultdict(str)
-            for index in range(len(words)):
-                w = words[index]
-                contexts[w] += " "+ ' '.join(words[index-wsize:index+wsize+1])
+            #TODO remove
+            if (counter + 1) % 100 == 0:
+                break
 
-            contexts.pop("_PAD_", None)
-            # Counting the words in the context of each word and adding them to its global counts
-            for w in contexts:
-                # Counting the context words
-                context_counts = collections.Counter(contexts[w].split())
-                # Removing the sentence paddings
-                context_counts.pop("_PAD_", None)
+            words = [token for token in doc.split() \
+                    if 2 <= len(token) <= 15 and not token in stoplist]
+            doc_len = len(words)
+            # Collecing the context of each word in a document
+            for index, w in enumerate(words):
                 if not w in dictionary:
                     dictionary[w] = len(dictionary)
+                    reversed_dictionary[dictionary[w]] = w
                 w_index = dictionary[w]
-                for cw, count in context_counts.items():
+                word_count[w_index] += 1
+
+                context = words[max(0, index-wsize): min(doc_len, index+wsize+1)]
+                # Adding context to w's bigram counts
+                for cw in context:
                     if not cw in dictionary:
                         dictionary[cw] = len(dictionary)
+                        reversed_dictionary[dictionary[cw]] = cw
                     cw_index = dictionary[cw]
-                    global_counts[w_index][cw_index] += count
-                #    print(w_index, w, cw, global_counts[w_index][cw_index])
-    return global_counts, dictionary
+                    bigram_counts[w_index][cw_index] += 1
+                #    print(w_index, w, cw, bigram_counts[w_index][cw_index])
+
+    return bigram_counts, word_counts, dictionary, reversed_dictionary
+
+
 
 if __name__ == "__main__":
-
     # Read file path for different resources
     input_file = sys.argv[1]
     output_file = sys.argv[2]
+    stopword_file = sys.argv[3]
     wsize = 5 # The size of the context window from each side
+    vocabulary_size = 100000 #The maximum number of vocabular to keep
 
-    global_counts, word2index = get_positive_context(input_file, wsize)
+    # Read the stop word list
+    stoplist = get_stopwords(stopword_file)
 
-    vocab_count = [global_counts[i][i] for i in global_counts]
-    print("vocab count", vocab_count)
+    bigram_counts, word_count, word2index, index2word = get_positive_examples(input_file, wsize, stoplist)
 
-    words_str= ""
+    sort_vocab = sorted(word_count.items(), key=lambda x: x[1])
+
+    print("sort_vocab", sort_vocab)
+
+    #cum_dist = cumulative_dist(vocab_counts)
+    #negative_counts = get_negative_examples(cum_dist, bigram_counts, len(vocab_counts))
+
+    #for w_index in bigram_counts.keys():
+    #    print("neg", w_index, sorted(negative_counts[w_index]))
+    #    print("pos", w_index, sorted(bigram_counts[w_index]))
+
+
     # Writing the positive examples and the dictionary
-    idf = open(output_file + "postive_ids", 'w')
-    cntf= open(output_file + "postive_counts", 'w')
-    for w in word2index.keys():
-        w_index = word2index[w]
-        w_ids = ""
-        w_counts = ""
-        words_str += "%s %d %d\n" % (w, w_index, global_counts[w_index][w_index])
-        for cw_index in global_counts[w_index]:
-            w_ids += "%d " % cw_index
-            w_counts += "%d " % global_counts[w_index][cw_index]
-        idf.write(w_ids + "\n")
-        cntf.write(w_counts + "\n")
+    idf = open(output_file + str(wsize) + "w_postive_ids", 'w')
+    cntf= open(output_file + str(wsize) + "w_postive_counts", 'w')
+    #neg_idf = open(output_file + str(wsize) + "w_negative_ids", 'w')
+    #neg_cntf = open(output_file + str(wsize) + "w_negative_counts", 'w')
+    words_str= ""
+    for w_index in range(len(word2index)):
+        w = index2word[w_index]
+        words_str += "%s %d %d\n" % (w, w_index, bigram_counts[w_index][w_index])
+        # Writing the positive files
+        ids, counts = "",""
+        for cw_index in bigram_counts[w_index]:
+            ids += "%d " % cw_index
+            counts += "%d " % bigram_counts[w_index][cw_index]
+        idf.write(ids + "\n")
+        cntf.write(counts + "\n")
+        # Writing the negative files
+        ids, counts = "", ""
+        #for nw_index in negative_counts[w_index]:
+        #    ids += "%d " % nw_index
+        #    counts += "%d " % negative_counts[w_index][nw_index]
+        #neg_idf.write(ids + "\n")
+        #neg_cntf.write(counts + "\n")
+
     idf.close()
     cntf.close()
+    #neg_idf.close()
+    #neg_cntf.close()
 
     with open(output_file + "word2id", 'w') as w2idf:
         w2idf.write(words_str)
-
-"""
-        print("start writing context in file")
-        for word in context_word:
-            if len(context_word[word]) > 0:
-                context_file = open(context_dir + "/" + word, 'a')
-                context_file.write('\n'.join(context_word[word]))
-                context_word[word] = []
-                context_file.close()
-
-        #break
-"""
 
 
 
